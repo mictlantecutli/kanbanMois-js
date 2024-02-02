48d3066b633707c7813d0c3cd00483e779593631
mictlantecutli <moisesparra82@hotmail.com>
2024-02-02 08:11:18 -0600

    insert Task

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import Kanban from "./kanban.js";
 
-console.log(Kanban.getAllTask());
+//console.log(Kanban.getAllTask());
+//console.log(Kanban.getTasks(2));
+
+//There are two "Edit Kanban Project Lectures" because I save the project two times
+//This show up me the 'return'
+console.log(Kanban.insertTask(1, "Edit Kanban Project Lectures"));
--- kanban.js
+++ kanban.js
@@ -1,13 +1,46 @@
 //Here we are going to work with static functions
 export default class Kanban{
-  static getTask(columnId){
+  static getTasks(columnId){
+    //const data = read()[columnId];
+    //return data.tasks;
+    //ANOTHER WAY TO DO:--->
+    //we can access to a particula COLUMN
+    const data = read().find(column =>{
+      return column.columnId == columnId
+    });
+
+    if(!data){
+      return [];
+    }
+    
+    return data.tasks;
+
 
 
   }
 
   static insertTask(columnId, content){
+    const data = read();
+    const column = data.find(column => {
+      return column.columnId == columnId;
+    });
+    const task= {
+      taskId: Math.floor(Math.random() * 100000),
+      content: content
+    };
 
+    //If the column does not exist. This goes after the finding
+    //But we do not use it because this function will depend on the event listener
+    /*if(!column){
+      throw new Error("Column doesn't exist!")
+    }*/
+
+    column.tasks.push(task);
+    console.log(data);
+    localStorage.setItem("data", JSON.stringify(data));
+    return  task;
   }
+
   static updateTask(taskId, updatedInformation){
 
   }
